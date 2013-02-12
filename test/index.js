873170ba5cbb09c1ae64abfd960595b25e31e398
Christopher Jeffrey <chjjeffrey@gmail.com>
2013-02-12 01:17:28 -0600

    remove test/fix and test/fix.js.

--- test/index.js
+++ test/index.js
@@ -110,6 +110,8 @@ main:
 
     for (; j < l; j++) {
       if (text[j] !== html[j]) {
+        failed++;
+
         text = text.substring(
           Math.max(j - 30, 0),
           Math.min(j + 30, text.length));
@@ -117,8 +119,6 @@ main:
         html = html.substring(
           Math.max(j - 30, 0),
           Math.min(j + 30, html.length));
-
-        failed++;
 
         console.log(
           '\n#%d. %s failed at offset %d. Near: "%s".\n',
@@ -281,6 +281,84 @@ function time(options) {
 }
 
 /**
+ * Markdown Test Suite Fixer
+ *   This function is responsible for "fixing"
+ *   the markdown test suite. There are
+ *   certain aspects of the suite that
+ *   are strange or might make tests
+ *   fail for reasons unrelated to
+ *   conformance.
+ */
+
+function fix(options) {
+  ['tests', 'original', 'new'].forEach(function(dir) {
+    try {
+      fs.mkdirSync(path.resolve(__dirname, dir), 0755);
+    } catch (e) {
+      ;
+    }
+  });
+
+  // rm -rf tests
+  fs.readdirSync(path.resolve(__dirname, 'tests')).forEach(function(file) {
+    fs.unlinkSync(path.resolve(__dirname, 'tests', file));
+  });
+
+  // cp -r original tests
+  fs.readdirSync(path.resolve(__dirname, 'original')).forEach(function(file) {
+    fs.writeFileSync(path.resolve(__dirname, 'tests', file),
+      fs.readFileSync(path.resolve(__dirname, 'original', file)));
+  });
+
+  // node fix.js
+  var dir = __dirname + '/tests';
+
+  // fix unencoded quotes
+  fs.readdirSync(dir).filter(function(file) {
+    return path.extname(file) === '.html';
+  }).forEach(function(file) {
+    var file = path.join(dir, file)
+      , html = fs.readFileSync(file, 'utf8');
+
+    html = html
+      .replace(/='([^\n']*)'(?=[^<>\n]*>)/g, '=&__APOS__;$1&__APOS__;')
+      .replace(/="([^\n"]*)"(?=[^<>\n]*>)/g, '=&__QUOT__;$1&__QUOT__;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      .replace(/&__QUOT__;/g, '"')
+      .replace(/&__APOS__;/g, '\'');
+
+    fs.writeFileSync(file, html);
+  });
+
+  // turn <hr /> into <hr>
+  fs.readdirSync(dir).forEach(function(file) {
+    var file = path.join(dir, file)
+      , text = fs.readFileSync(file, 'utf8');
+
+    text = text.replace(/(<|&lt;)hr\s*\/(>|&gt;)/g, '$1hr$2');
+
+    fs.writeFileSync(file, text);
+  });
+
+  // markdown does some strange things.
+  // it does not encode naked `>`, marked does.
+  (function() {
+    var file = dir + '/amps_and_angles_encoding.html';
+    var html = fs.readFileSync(file, 'utf8')
+      .replace('6 > 5.', '6 &gt; 5.');
+
+    fs.writeFileSync(file, html);
+  })();
+
+  // cp new/* tests/
+  fs.readdirSync(path.resolve(__dirname, 'new')).forEach(function(file) {
+    fs.writeFileSync(path.resolve(__dirname, 'tests', file),
+      fs.readFileSync(path.resolve(__dirname, 'new', file)));
+  });
+}
+
+/**
  * Argument Parsing
  */
 
@@ -321,6 +399,11 @@ function parseArg(argv) {
   while (argv.length) {
     arg = getarg();
     switch (arg) {
+      case '-f':
+      case '--fix':
+      case 'fix':
+        options.fix = true;
+        break;
       case '-b':
       case '--bench':
         options.bench = true;
@@ -375,6 +458,10 @@ function camelize(text) {
 
 function main(argv) {
   var opt = parseArg();
+
+  if (opt.fix) {
+    return fix(opt);
+  }
 
   if (opt.bench) {
     return runBench(opt);
