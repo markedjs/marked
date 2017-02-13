ff2735f22ff721c0701079be5d41130122ecf970
Rumkin <dev@rumk.in>
2017-02-13 09:36:25 +0000

    add exmplates, set plugin's this to renderer

---
+++ examples/plugin.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var marked = require('marked');
+var csv = require('csv-string');
+var html = require('escape-html');
+
+// Enable plugins for all instances
+marked.setOptions({plugins: true});
+
+// Initialize renderer
+var renderer = new marked.Renderer({
+  plugins: {
+    // Convert `@link(title,url)` into link.
+    links: function(params, block) {
+      var parts = params.split(/\s*,\s*/);
+      return '<a href="' + html(parts[1]) + '">' + html(parts[0]) + '</a>';
+    },
+
+    // Convert `@github(user/repo)` into github link.
+    github: function(params, block) {
+      return '<a href="https://github.com/' + html(params) + '">' + html(params) + '</a>';
+    },
+
+    // Convert `@csv(headers)` block data into table.
+    csv: function(params, block) {
+      var thead = params.length ? params.split(/\s*,\s*/) : null;
+      var tbody = csv.parse(block);
+
+      var table = ['<table>']; // Use array to avoid string reallocations
+      if (thead) {
+        table.push('<thead><tr>');
+        thead.forEach(function(heading) {
+          table.push('<th>' + html(heading) + "</th>");
+        });
+        table.push('</tr></thead>');
+      }
+      table.push('<tbody>');
+      tbody.forEach(function(row) {
+        table.push('<tr>');
+        row.forEach(function(cell) {
+          table.push('<td>' + html(cell) + "</td>");
+        });
+        table.push('</tr>');
+      });
+      table.push('</tbody></table>');
+
+      return table.join('\n');
+    },
+  }
+});
+
+// Add plugin to initialized renderer.
+// Parse block of markdown inside markdown. Yep even so :)
+renderer.plugins.markdown = function(params, block) {
+  return marked(block, {renderer: this});
+};
