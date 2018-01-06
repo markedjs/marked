b61af2a2c30650511dfa23ae54adbc6d17f9b827
Tony Brix <tony@brix.ninja>
2018-01-06 10:46:21 -0600

    clean up

--- test/index.js
+++ test/index.js
@@ -342,9 +342,10 @@ function fix() {
   fs.readdirSync(path.resolve(__dirname, 'original')).forEach(function(file) {
     var text = fs.readFileSync(path.resolve(__dirname, 'original', file));
 
-    if (file.indexOf('hard_wrapped_paragraphs_with_list_like_lines.md') === 0) {
+    if (file === 'hard_wrapped_paragraphs_with_list_like_lines.md') {
       text = '---\ngfm: false\n---\n' + text;
     }
+
     fs.writeFileSync(path.resolve(__dirname, 'compiled_tests', file), text);
   });
 
