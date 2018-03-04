d74044cb6985fb3075d8becb3f76faca32283d83
Jamie Davis <davisjam@vt.edu>
2018-03-04 15:05:32 -0500

    test: inline link tests
    These are based on test/original/links_inline_style.md

--- test/integration/marked-spec.js
+++ test/integration/marked-spec.js
@@ -1,5 +1,62 @@
 var marked = require('../../marked.min.js');
 
-it('should run the test', function () {
-  expect(marked('Hello World!')).toBe('<p>Hello World!</p>\n');
+describe('integration suite', function () {
+  it('should put plaintext into a paragraph', function () {
+    expect(marked('Hello World!')).toBe('<p>Hello World!</p>\n');
+  });
+
+  describe('links inline style', function () {
+
+    it('should handle regular links', function () {
+      var descs = [{'text': 'simpleString',
+                    'link': 'simpleString'},
+                   {'text': 'string with spaces',
+                    'link': 'http://github.com'},
+                   {'text': 'string with \[backslash \] brackets',
+                    'link': 'http://github.com'},
+                   {'text': 'string with [inline brackets]',
+                    'link': 'http://github.com'},
+                   {'text': 'url has space',
+                    'link': '/url has space'},
+                   {'text': 'empty',
+                    'link': ''},
+                  ];
+      descs.forEach(function(desc) {
+        expect(marked(`[${desc.text}](${desc.link})`)).toBe(`<p><a href="${desc.link}">${desc.text}</a></p>\n`);
+      });
+    });
+
+    it('should handle links with titles', function () {
+      var descs = [{'text': 'simpleString',
+                    'link': 'simpleString',
+                    'title': 'simpleTitle',
+                    'beforeTitle': ' ',
+                    'afterTitle': ''},
+                   {'text': 'url and title',
+                    'link': 'simpleString',
+                    'title': 'title preceded by two spaces',
+                    'beforeTitle': '  ',
+                    'afterTitle': ''},
+                   {'text': 'url and title',
+                    'link': 'simpleString',
+                    'title': 'title preceded by a tab',
+                    'beforeTitle': '	',
+                    'afterTitle': ''},
+                   {'text': 'url and title',
+                    'link': 'simpleString',
+                    'title': 'title has spaces afterward',
+                    'beforeTitle': ' ',
+                    'afterTitle': '  '},
+                   {'text': 'url and title',
+                    'link': '/url/has space/',
+                    'title': 'url has space and title',
+                    'beforeTitle': ' ',
+                    'afterTitle': ''},
+                  ];
+      descs.forEach(function(desc) {
+        expect(marked(`[${desc.text}](${desc.link}${desc.beforeTitle}"${desc.title}"${desc.afterTitle})`)).toBe(`<p><a href="${desc.link}" title="${desc.title}">${desc.text}</a></p>\n`);
+      });
+    });
+
+  });
 });
