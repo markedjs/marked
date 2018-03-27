0c026168d70f4550c10f7869c6e320f34a5ea526
Josh Bruce <josh@joshbruce.com>
2018-03-26 20:48:18 -0400

    624 test - 407 failures

--- test/integration/marked-spec.js
+++ test/integration/marked-spec.js
@@ -1,18 +1,29 @@
 var marked = require('../../marked.min.js');
+var cmSpec = require('./commonmark.json');
 
-it('should run the test', function () {
-  expect(marked('Hello World!')).toBe('<p>Hello World!</p>\n');
+describe('CommonMark 0.28', function() {
+	cmSpec.forEach(function(example) {
+		var consoleString = 'should pass example ' + example.example;
+		it(consoleString, function() {
+			expect(
+				marked(example.markdown)
+			).toBe(example.html);
+		});
+	});
 });
+// it('should run the test', function () {
+//   expect(marked('Hello World!')).toBe('<p>Hello World!</p>\n');
+// });
 
-// http://spec.commonmark.org/0.28/#example-230
-it('should start an ordered list at 0 when requested', function () {
-  expect(
-    marked('0. ok')).
-    toBe("<ol start=\"0\">\n<li>ok</li>\n</ol>\n")
-});
+// // http://spec.commonmark.org/0.28/#example-230
+// it('should start an ordered list at 0 when requested', function () {
+//   expect(
+//     marked('0. ok')).
+//     toBe("<ol start=\"0\">\n<li>ok</li>\n</ol>\n")
+// });
 
-// http://spec.commonmark.org/0.28/#example-234
-it('indents code within an explicitly-started ordered list', function () {
-  expect(marked("  10.  foo\n\n           bar")).
-  toBe("<ol start=\"10\">\n<li><p>foo</p>\n<pre><code>bar\n</code></pre></li>\n</ol>\n");
-});
+// // http://spec.commonmark.org/0.28/#example-234
+// it('indents code within an explicitly-started ordered list', function () {
+//   expect(marked("  10.  foo\n\n           bar")).
+//   toBe("<ol start=\"10\">\n<li><p>foo</p>\n<pre><code>bar\n</code></pre></li>\n</ol>\n");
+// });
