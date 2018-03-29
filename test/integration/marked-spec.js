b2cd9b765872c6e8217e3991ca8f1e9e06b4d18e
Josh Bruce <josh@joshbruce.com>
2018-03-28 23:29:40 -0400

    Verbiage

--- test/integration/marked-spec.js
+++ test/integration/marked-spec.js
@@ -6,13 +6,13 @@ var since = require('jasmine2-custom-message');
 describe('CommonMark 0.28', function() {
 	var htmlDiffer = new HtmlDiffer();
 	var failures = [];
-	cmSpec.forEach(function(example) {
-		var consoleString = 'should pass example ' + example.example;
+	cmSpec.forEach(function(spec) {
+		var consoleString = 'should pass example ' + spec.example;
 
 		// Remove XHTML closing slash (~30 examples from CM spec use HTML explicit closure)
-		var expected = example.html.replace(' />', '>');
+		var expected = spec.html.replace(' />', '>');
 
-		var actual = sut(example.markdown);
+		var actual = sut(spec.markdown);
 
 		var message = 'CommonMark:\n' + expected + '\n------\n\nMarked:\n' + actual;
 
