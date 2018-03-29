ada4a1d8c0ffa281260e0c3863b60eafee5da0c5
Tony Brix <tony@brix.ninja>
2018-03-29 13:00:59 -0500

    lint all js files

--- test/integration/marked-spec.js
+++ test/integration/marked-spec.js
@@ -6,13 +6,12 @@ it('should run the test', function () {
 
 // http://spec.commonmark.org/0.28/#example-230
 it('should start an ordered list at 0 when requested', function () {
-  expect(
-    marked('0. ok')).
-    toBe("<ol start=\"0\">\n<li>ok</li>\n</ol>\n")
+  expect(marked('0. ok'))
+    .toBe('<ol start="0">\n<li>ok</li>\n</ol>\n')
 });
 
 // http://spec.commonmark.org/0.28/#example-234
 it('indents code within an explicitly-started ordered list', function () {
-  expect(marked("  10.  foo\n\n           bar")).
-  toBe("<ol start=\"10\">\n<li><p>foo</p>\n<pre><code>bar\n</code></pre></li>\n</ol>\n");
+  expect(marked('  10.  foo\n\n           bar'))
+    .toBe('<ol start="10">\n<li><p>foo</p>\n<pre><code>bar\n</code></pre></li>\n</ol>\n');
 });
