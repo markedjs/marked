90146b73599571c26d5fcaffcae8945ea7fdd3bd
Josh Bruce <josh@joshbruce.com>
2018-03-28 23:17:32 -0400

    Clean up

--- test/integration/marked-spec.js
+++ test/integration/marked-spec.js
@@ -7,11 +7,16 @@ describe('CommonMark 0.28', function() {
 	var htmlDiffer = new HtmlDiffer();
 	cmSpec.forEach(function(example) {
 		var consoleString = 'should pass example ' + example.example;
+
+		// Remove XHTML closing slash (~30 examples from CM spec use HTML explicit closure)
 		var expected = example.html.replace(' />', '>');
+
 		var actual = sut(example.markdown);
-		// sut.setOptions({xhtml:true});
+
+		var message = 'CommonMark:\n' + expected + '\n------\n\nMarked:\n' + actual;
+
 		it(consoleString, function() {
-			since('CommonMark:\n' + expected + '\n------\n\nMarked:\n' + actual).expect(
+			since(message).expect(
 				htmlDiffer.isEqual(expected, actual)				
 			).toEqual(true);
 		});
