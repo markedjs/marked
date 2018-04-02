a67c8f67127b2b2b40ece2587c574985a6eb203e
Josh Bruce <josh@joshbruce.com>
2018-04-02 18:43:37 -0400

    Post review

--- test/integration/marked-spec.js
+++ test/integration/marked-spec.js
@@ -1,7 +1,4 @@
 var marked = require('../../marked.min.js');
-var HtmlDiffer = require('html-differ').HtmlDiffer,
-    htmlDiffer = new HtmlDiffer();
-var since = require('jasmine2-custom-message');
 
 it('should run the test', function () {
   expect(marked('Hello World!')).toBe('<p>Hello World!</p>\n');
--- test/unit/marked-spec.js
+++ test/unit/marked-spec.js
@@ -1,20 +1,20 @@
-var sut = require('../../lib/marked.js');
+var marked = require('../../lib/marked.js');
 
 it('should run the test', function () {
-  spyOn(sut, 'parse').and.callThrough();
-  sut.parse('Hello World!');
-  expect(sut.parse).toHaveBeenCalled();
+  spyOn(marked, 'parse').and.callThrough();
+  marked.parse('Hello World!');
+  expect(marked.parse).toHaveBeenCalled();
 });
 
 describe('Test heading ID functionality', function() {
 	it('should add id attribute by default', function() {
-		var renderer = new sut.Renderer(sut.defaults);
+		var renderer = new marked.Renderer(marked.defaults);
 		var header = renderer.heading('test', 1, 'test');
 		expect(header).toBe('<h1 id="test">test</h1>\n');
 	});
 
 	it('should NOT add id attribute when options set false', function() {
-		var renderer = new sut.Renderer({ headerIds: false });
+		var renderer = new marked.Renderer({ headerIds: false });
 		var header = renderer.heading('test', 1, 'test');
 		expect(header).toBe('<h1>test</h1>\n');
 	});
