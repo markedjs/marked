83806c09956a76229c1c653a6e0f68ac4b25e2ef
Josh Bruce <josh@joshbruce.com>
2018-04-02 15:13:19 -0400

    Make unit tests

--- test/unit/marked-spec.js
+++ test/unit/marked-spec.js
@@ -1,7 +1,21 @@
-var marked = require('../../lib/marked.js');
+var sut = require('../../lib/marked.js');
 
 it('should run the test', function () {
-  spyOn(marked, 'parse').and.callThrough();
-  marked.parse('Hello World!');
-  expect(marked.parse).toHaveBeenCalled();
+  spyOn(sut, 'parse').and.callThrough();
+  sut.parse('Hello World!');
+  expect(sut.parse).toHaveBeenCalled();
+});
+
+describe('Test heading ID functionality', function() {
+	it('should add id attribute by default', function() {
+		var renderer = new sut.Renderer(sut.defaults);
+		var header = renderer.heading('test', 1, 'test');
+		expect(header).toBe('<h1 id="test">test</h1>\n');
+	});
+
+	it('should NOT add id attribute when options set false', function() {
+		var renderer = new sut.Renderer({ headerIds: false });
+		var header = renderer.heading('test', 1, 'test');
+		expect(header).toBe('<h1>test</h1>\n');
+	});
 });
