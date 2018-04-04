00fcc4f5e9dc165d4c9c27fa6c4c27b7ce87348c
Josh Bruce <josh@joshbruce.com>
2018-04-03 21:23:25 -0400

    Testing options and defaults

--- test/unit/marked-spec.js
+++ test/unit/marked-spec.js
@@ -7,15 +7,30 @@ it('should run the test', function () {
 });
 
 describe('Test heading ID functionality', function() {
-	it('should add id attribute by default', function() {
-		var renderer = new marked.Renderer(marked.defaults);
-		var header = renderer.heading('test', 1, 'test');
-		expect(header).toBe('<h1 id="test">test</h1>\n');
-	});
+  it('should add id attribute by default', function() {
+    var renderer = new marked.Renderer(marked.defaults);
+    var header = renderer.heading('test', 1, 'test');
+    expect(header).toBe('<h1 id="test">test</h1>\n');
+  });
 
-	it('should NOT add id attribute when options set false', function() {
-		var renderer = new marked.Renderer({ headerIds: false });
-		var header = renderer.heading('test', 1, 'test');
-		expect(header).toBe('<h1>test</h1>\n');
-	});
+  it('should NOT add id attribute when options set false', function() {
+    var renderer = new marked.Renderer({ headerIds: false });
+    var header = renderer.heading('test', 1, 'test');
+    expect(header).toBe('<h1>test</h1>\n');
+  });
+});
+
+describe('Test options', function() {
+  it('headerIds defaults should be true', function() {
+    expect(marked.defaults.headerIds).toBe(true);
+  });
+
+  it('headerIds options should be true', function() {
+    expect(marked.options.headerIds).toBe(true);
+  });
+
+  it('can change headerIds option to false', function() {
+    marked.defaults.headerIds = false;
+    expect(marked.defaults.headerIds).toBe(false);
+  });
 });
