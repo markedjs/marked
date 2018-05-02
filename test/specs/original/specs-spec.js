3b9ea4cc49d499da6121d3468a33be20cf23f70b
Tony Brix <tony@brix.ninja>
2018-05-02 08:34:33 -0500

    add tests

--- test/specs/original/specs-spec.js
+++ test/specs/original/specs-spec.js
@@ -1,4 +1,5 @@
-var specTests = require('../../');
+var marked = require('../../../lib/marked.js');
+var specTests = require('../../index.js');
 
 it('should run spec tests', function () {
   // hide output
@@ -9,4 +10,21 @@ it('should run spec tests', function () {
     specTests();
     fail();
   }
+});
+
+it('should use the correct paragraph type', function () {
+  const md = `
+A Paragraph.
+
+> A blockquote
+
+- list item
+
+`;
+
+  const tokens = marked.lexer(md);
+
+  expect(tokens[0].type).toBe('paragraph');
+  expect(tokens[3].type).toBe('paragraph');
+  expect(tokens[7].type).toBe('text');
 });
