7a2ee5cbfe936d1bbdbe8aee51a6c666f10ddc45
43081j <43081j@users.noreply.github.com>
2018-10-03 19:20:14 +0100

    pass through babel during bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,11 @@
+import babel from 'rollup-plugin-babel';
+
 export default {
   input: 'src/marked.js',
   output: {
     file: 'marked.min.js',
     format: 'umd',
     name: 'marked'
-  }
+  },
+  plugins: [babel()]
 }
