7c0873708268e88ed4d094c9022f63002f63d2fa
Tony Brix <tony@brix.ninja>
2019-04-09 14:35:02 -0500

    use node-fetch

--- test/specs/commonmark/getSpecs.js
+++ test/specs/commonmark/getSpecs.js
@@ -1,21 +1,24 @@
-const request = require('request-promise');
+const fetch = require('node-fetch');
 const marked = require('../../../');
 const htmlDiffer = require('../../helpers/html-differ.js');
-const fs = require("fs");
+const fs = require('fs');
 
-(async () => {
-  const pkg = JSON.parse(await request('https://raw.githubusercontent.com/commonmark/commonmark.js/master/package.json'));
-  const version = pkg.version.replace(/^(\d+\.\d+).*$/, '$1');
-  const specs = JSON.parse(await request(`https://spec.commonmark.org/${version}/spec.json`));
-  const newSpecs = specs.map((spec => {
-    const newspec = {...spec};
-    const html = marked(spec.markdown, {headerIds: false});
-    if (!htmlDiffer.isEqual(html, spec.html)) {
-      newspec.shouldFail = true;
-    }
-    return newspec;
-  }));
-  fs.writeFileSync(`commonmark.${version}.json`, JSON.stringify(newSpecs, null, 2) + '\n')
-})().catch((err) => {
-  console.error(err);
-});
+fetch('https://raw.githubusercontent.com/commonmark/commonmark.js/master/package.json')
+  .then(res => res.json())
+  .then(pkg => pkg.version.replace(/^(\d+\.\d+).*$/, '$1'))
+  .then(version =>
+    fetch(`https://spec.commonmark.org/${version}/spec.json`)
+      .then(res => res.json())
+      .then(specs => {
+        specs.forEach(spec => {
+          const html = marked(spec.markdown, {headerIds: false});
+          if (!htmlDiffer.isEqual(html, spec.html)) {
+            spec.shouldFail = true;
+          }
+        });
+        fs.writeFileSync(`commonmark.${version}.json`, JSON.stringify(specs, null, 2) + '\n');
+      })
+  )
+  .catch((err) => {
+    console.error(err);
+  });
