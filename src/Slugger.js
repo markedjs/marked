193a41e78d625765075f652a152366e1e1e17435
Tony Brix <tony@brix.ninja>
2020-03-21 22:05:06 -0500

    simplify tag regex

--- src/Slugger.js
+++ src/Slugger.js
@@ -14,7 +14,7 @@ module.exports = class Slugger {
       .toLowerCase()
       .trim()
       // remove html tags
-      .replace(/<!?\/?[\w-]+(?: .*)?\/?>/g, '')
+      .replace(/<[!\/a-z].*?>/ig, '')
       // remove unwanted chars
       .replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,./:;<=>?@[\]^`{|}~]/g, '')
       .replace(/\s/g, '-');
