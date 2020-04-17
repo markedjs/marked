70dc21f4a1c47154e96862d67438db4730913d06
Ivan Demidov <Scrum@list.ru>
2020-04-17 09:28:53 +0300

    test: perf toRenderExact method

--- test/helpers/helpers.js
+++ test/helpers/helpers.js
@@ -43,14 +43,9 @@ beforeEach(() => {
       compare: async(spec, expected) => {
         const result = {};
         const actual = marked(spec.markdown, spec.options);
+        
         result.pass = assert.deepStrictEqual(expected, actual) === undefined;
 
-        if (result.pass) {
-          result.message = `${spec.markdown}\n------\n\nExpected: Should Fail`;
-        } else {
-          const diff = await htmlDiffer.firstDiff(actual, expected);
-          result.message = `Expected: ${diff.expected}\n  Actual: ${diff.actual}`;
-        }
         return result;
       }
     })
