bad2a92823d5cf8e0197ec4ff0820c74dc4917dc
Steven <steven@ceriously.com>
2020-08-09 14:16:16 -0400

    Run lint

--- api/dingus.js
+++ api/dingus.js
@@ -3,15 +3,15 @@ const version = require('../package.json').version;
 const name = 'Marked.js';
 
 module.exports = (req, res) => {
-	if (req.method !== 'GET') {
-		return res.status(405).json({
-        error: {
-          code: 'method_not_allowed',
-          message: 'Only GET requests are supported for this endpoint.',
-        }
-      });
-	}
-	const { text = '' } = req.query;
-	const html = marked(text);
+  if (req.method !== 'GET') {
+    return res.status(405).json({
+      error: {
+        code: 'method_not_allowed',
+        message: 'Only GET requests are supported for this endpoint.'
+      }
+    });
+  }
+  const { text = '' } = req.query;
+  const html = marked(text);
   res.json({ name, version, html });
-}
+};
