9d90c8340f919f251c6ef98721d361c57f23e46e
Tony Brix <tony@brix.ninja>
2020-09-28 15:22:48 -0500

    release with semantic release

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+    '@semantic-release/git'
+  ]
+};
