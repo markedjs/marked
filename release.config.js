7fc94731a060c5c70e5d11d5d9ff8b099de45252
Tony Brix <tony@brix.ninja>
2020-09-28 17:51:31 -0500

    remove @semantic-release/changelog

--- release.config.js
+++ release.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/github',
     '@semantic-release/git'
