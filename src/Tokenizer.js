b7eea9504bd73872274dbb938ae2baa7c57b6e0e
Tony Brix <tony@brix.ninja>
2022-11-20 10:05:52 -0600

    fix: fix link reference definitions specs (#2654)

--- src/Tokenizer.js
+++ src/Tokenizer.js
@@ -365,14 +365,15 @@ export class Tokenizer {
   def(src) {
     const cap = this.rules.block.def.exec(src);
     if (cap) {
-      if (cap[3]) cap[3] = cap[3].substring(1, cap[3].length - 1);
       const tag = cap[1].toLowerCase().replace(/\s+/g, ' ');
+      const href = cap[2] ? cap[2].replace(/^<(.*)>$/, '$1').replace(this.rules.inline._escapes, '$1') : '';
+      const title = cap[3] ? cap[3].substring(1, cap[3].length - 1).replace(this.rules.inline._escapes, '$1') : cap[3];
       return {
         type: 'def',
         tag,
         raw: cap[0],
-        href: cap[2] ? cap[2].replace(this.rules.inline._escapes, '$1') : cap[2],
-        title: cap[3] ? cap[3].replace(this.rules.inline._escapes, '$1') : cap[3]
+        href,
+        title
       };
     }
   }
@@ -574,7 +575,7 @@ export class Tokenizer {
         || (cap = this.rules.inline.nolink.exec(src))) {
       let link = (cap[2] || cap[1]).replace(/\s+/g, ' ');
       link = links[link.toLowerCase()];
-      if (!link || !link.href) {
+      if (!link) {
         const text = cap[0].charAt(0);
         return {
           type: 'text',
