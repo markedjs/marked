886de34853dc3ce10e39642548ca75521c6cfea2
Tristan F <LeoDog896@hotmail.com>
2023-01-13 21:27:58 -0500

    chore(ci): add version, unify output (#2701)
    Co-authored-by: Tony Brix <tony@brix.ninja>
    fixes undefined

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
 import babel from '@rollup/plugin-babel';
+import { defineConfig } from 'rollup';
+import fs from 'fs';
+
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
 
 const banner = `/**
- * marked - a markdown parser
+ * marked ${process.env.SEMANTIC_RELEASE_NEXT_VERSION || pkg.version} - a markdown parser
  * Copyright (c) 2011-${new Date().getFullYear()}, Christopher Jeffrey. (MIT Licensed)
  * https://github.com/markedjs/marked
  */
@@ -12,7 +16,7 @@ const banner = `/**
  */
 `;
 
-export default [{
+export default defineConfig([{
   input: 'src/marked.js',
   output: {
     file: 'lib/marked.esm.js',
@@ -22,29 +26,21 @@ export default [{
 },
 {
   input: 'src/marked.js',
-  output: {
+  output: [{
     file: 'lib/marked.umd.js',
     format: 'umd',
     name: 'marked',
     banner
   },
-  plugins: [
-    babel({
-      presets: [['@babel/preset-env', { loose: true }]]
-    })
-  ]
-},
-{
-  input: 'src/marked.js',
-  output: {
+  {
     file: 'lib/marked.cjs',
     format: 'cjs',
     name: 'marked',
     banner
-  },
+  }],
   plugins: [
     babel({
       presets: [['@babel/preset-env', { loose: true }]]
     })
   ]
-}];
+}]);
