587729f9a5721369ace1d1ad0d429b68022d55a6
Tony Brix <tony@brix.ninja>
2023-09-29 01:54:12 -0500

    chore: remove node 4 polyfills (#3014)

--- test/helpers/load.js
+++ test/helpers/load.js
@@ -5,47 +5,6 @@ import { createRequire } from 'module';
 
 const require = createRequire(import.meta.url);
 
-function node4Polyfills() {
-  // https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padEnd
-  if (!String.prototype.padEnd) {
-    // eslint-disable-next-line no-extend-native
-    String.prototype.padEnd = function padEnd(targetLength, padString) {
-      targetLength = targetLength >> 0; // floor if number or convert non-number to 0;
-      padString = String((typeof padString !== 'undefined' ? padString : ' '));
-      if (this.length > targetLength) {
-        return String(this);
-      } else {
-        targetLength = targetLength - this.length;
-        if (targetLength > padString.length) {
-          padString += padString.repeat(targetLength / padString.length); // append to original to ensure we are longer than needed
-        }
-        return String(this) + padString.slice(0, targetLength);
-      }
-    };
-  }
-
-  // https://github.com/uxitten/polyfill/blob/master/string.polyfill.js
-  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
-  if (!String.prototype.padStart) {
-    // eslint-disable-next-line no-extend-native
-    String.prototype.padStart = function padStart(targetLength, padString) {
-      targetLength = targetLength >> 0; // truncate if number, or convert non-number to 0;
-      padString = String(typeof padString !== 'undefined' ? padString : ' ');
-      if (this.length >= targetLength) {
-        return String(this);
-      } else {
-        targetLength = targetLength - this.length;
-        if (targetLength > padString.length) {
-          padString += padString.repeat(targetLength / padString.length); // append to original to ensure we are longer than needed
-        }
-        return padString.slice(0, targetLength) + String(this);
-      }
-    };
-  }
-}
-node4Polyfills();
-
 export function outputCompletionTable(title, specs) {
   let longestName = 0;
   let maxSpecs = 0;
