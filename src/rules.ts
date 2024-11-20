fd015f147da064c40940491495cd06fc4476d178
WizardMeow <goldenkingppp@gmail.com>
2024-11-21 07:12:10 +0800

    fix: update punctuation regex syntax for compatibility (#3540)
    change the string to a regular expression. Then babel can transpile it correctly.

--- src/rules.ts
+++ src/rules.ts
@@ -243,7 +243,7 @@ const br = /^( {2,}|\\)\n(?!\s*$)/;
 const inlineText = /^(`+|[^`])(?:(?= {2,}\n)|[\s\S]*?(?:(?=[\\<!\[`*_]|\b_|$)|[^ ](?= {2,}\n)))/;
 
 // list of unicode punctuation marks, plus any missing characters from CommonMark spec
-const _punctuation = '\\p{P}\\p{S}';
+const _punctuation = /\p{P}\p{S}/u;
 const punctuation = edit(/^((?![*_])[\spunctuation])/, 'u')
   .replace(/punctuation/g, _punctuation).getRegex();
 
