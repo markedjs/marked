b7b0272ee4927ff77a2339a576068b1655d5d644
Frank Lemanschik <117739566+lemanschik@users.noreply.github.com>
2025-02-09 07:29:23 +0100

    Update dingus.js
    Fix: double quote replaced by single quote

--- api/dingus.js
+++ api/dingus.js
@@ -1,5 +1,5 @@
 import { marked } from '../lib/marked.esm.js';
-import pkg from "../package.json" with { type: "json" };
+import pkg from '../package.json' with { type: 'json' };
 
 const version = pkg.version;
 const name = 'Marked';
