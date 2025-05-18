a6d047ffdd6ecb341dad9023246cf1ff1d74c05e
Tony Brix <tony@brix.ninja>
2025-05-18 13:49:41 -0600

    BREAKING: remove cjs and build

--- test/cjs-test.cjs
+++ test/cjs-test.cjs
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const { marked } = require('../lib/marked.cjs');
+const { marked } = require('../lib/marked.esm.js');
 
 if (!marked.parse('# test').includes('<h1')) {
   throw new Error('Invalid markdown');
