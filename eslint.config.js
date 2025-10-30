f3e834def52b8f7fffd2215de39f5b4dce658b43
Tony Brix <tony@brix.ninja>
2025-10-30 10:01:23 -0600

    add vuln.js to eslint ignores

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import markedEslintConfig from '@markedjs/eslint-config';
 
 export default [
   {
-    ignores: ['**/lib', '**/public', 'test.js'],
+    ignores: ['**/lib', '**/public', 'test.js', 'vuln.js'],
   },
   ...markedEslintConfig,
 ];
