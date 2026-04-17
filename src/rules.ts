abd907aab53a9f33d3b50a26a1891776a999ca4e
christophe geiser <christophe-g@users.noreply.github.com>
2026-04-17 05:11:49 +0200

    fix(rules): ensure lookbehind regex is evaluated correctly by minifiers (#3945)
    * fix(rules): ensure lookbehind regex is evaluated correctly by minifiers
    
    * Use a default variable in the regex to prevent removal by minifiers
    
    * fix(rules): remove unnecessary eslint directive for lookbehind regex

--- src/rules.ts
+++ src/rules.ts
@@ -16,10 +16,10 @@ function edit(regex: string | RegExp, opt = '') {
   return obj;
 }
 
-const supportsLookbehind = (() => {
+// use a default variable to ensure the regex is evaluated on client an not removed by minifiers
+const supportsLookbehind = ((a = '') => {
 try {
-  // eslint-disable-next-line prefer-regex-literals
-  return !!new RegExp('(?<=1)(?<!1)');
+  return !!new RegExp('(?<=1)(?<!1)' + a);
 } catch {
   // See browser support here:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Regular_expressions/Lookbehind_assertion
